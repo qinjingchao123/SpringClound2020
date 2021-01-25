d571dc157c39c57d77858577a6c9ef239231f47d
qinjingchao <1540339349@qq.com>
2021-01-25 17:52:27 +0800

    修改

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -53,19 +53,6 @@ public class PaymentController
         {
             return new CommonResult(200,"查询成功,serverPort:  "+serverPort,payment);
         }else{
-
-
-
-
-
-
-
-
-
-
-
-
-
             return new CommonResult(444,"没有对应记录,查询ID: "+id,null);
         }
     }
